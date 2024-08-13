baf3c97040cddc1a65790fafd885862d5725f34a
kalosa0770 <kalosaelijah3@gmail.com>
2024-08-13 04:56:45 +0100

    second commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// Menu Mobile Bar
+function menuBar() {
+    const menubar = document.querySelector(".sideBar")
+    menubar.style.display = 'flex'
+}
+
+// Close Menu Bar
+function closeBar() {
+    const menubar = document.querySelector(".sideBar")
+    menubar.style.display = 'none'
+}
