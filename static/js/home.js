43017fe23d3696f4b586312052de7722630d1b9a
kalosa0770 <kalosaelijah3@gmail.com>
2024-08-20 03:18:24 +0100

    worked on home

--- static/js/home.js
+++ static/js/home.js
@@ -11,3 +11,41 @@ browseDropdown.addEventListener("click", function(){
     }
 });
 
+// settings account popup
+
+function settings() {
+    const settingsContent = document.getElementById("settings-content");
+    const close = document.getElementById("close");
+
+    if (settingsContent.style.display === "none" || settingsContent.style.display === "") {
+        settingsContent.style.display = "flex";
+    }
+    
+     else {
+        settingsContent.style.display = "none";
+    }
+
+    close.addEventListener("click", function(){
+        settingsContent.style.display = "none";
+    });
+}
+
+
+
+
+
+
+const catalogDropdown = document.getElementById("catalog");
+const catalogDropdownContent = document.getElementById("dropdown-catalog");
+
+catalogDropdown.addEventListener("click", function(){
+    if (catalogDropdownContent.style.display === "none" || catalogDropdownContent.style.display === "") {
+        catalogDropdownContent.style.display = "flex";
+    } else {
+        catalogDropdownContent.style.display = "none";
+    }
+});
+
+
+
+
